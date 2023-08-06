262ce4e5712a0549ec76c658d9ae8c56f221a566
chipulaja <chipul.aja@gmail.com>
2023-08-06 10:03:08 +0000

    test

--- src/symfony-maker-bundle/Resources/skeleton/controller/twig_template.tpl.php
+++ src/symfony-maker-bundle/Resources/skeleton/controller/twig_template.tpl.php
@@ -1,18 +1,9 @@
-<?= $helper->getHeadPrintCode("Hello $class_name!"); ?>
+{% extends 'layout-sbadmin.html.twig' %}
 
-{% block body %}
-<style>
-    .example-wrapper { margin: 1em auto; max-width: 800px; width: 95%; font: 18px/1.5 sans-serif; }
-    .example-wrapper code { background: #F5F5F5; padding: 2px 6px; }
-</style>
+{% block title %}{{ controller_name }}{% endblock %}
 
-<div class="example-wrapper">
-    <h1>Hello {{ controller_name }}! ✅ chipulaja</h1>
+{% block content %}
+    
+    Halo dunia
 
-    This friendly message is coming from:
-    <ul>
-        <li>Your controller at <code><?= $helper->getFileLink("$root_directory/$controller_path", "$controller_path"); ?></code></li>
-        <li>Your template at <code><?= $helper->getFileLink("$root_directory/$relative_path", "$relative_path"); ?></code></li>
-    </ul>
-</div>
 {% endblock %}
